46785c87caee58c7a6aa3f819774878fde00ad20
sigmarius <aamokeeva@yandex.ru>
2020-12-21 15:19:52 +0300

    Добавляет маску для номера телефона и плавный переход по якорным ссылкам

--- build/js/main.js
+++ build/js/main.js
@@ -144,3 +144,50 @@
   });
 
 })();
+
+// Плавная прокрутка до якоря
+(function () {
+  var links = document.querySelectorAll('[data-link]');
+
+  for (var i = 0; i < links.length; i++) {
+
+    links[i].addEventListener('click', function (evt) {
+
+      var target = document.querySelector('[id=' + evt.target.dataset.link + ']');
+
+      target.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
+  }
+
+})();
+
+
+// Валидация номера телефона
+(function () {
+  var phoneNumber = document.querySelectorAll('[type=tel]');
+
+  var setMask = function (input) {
+    input.addEventListener('focus', function () {
+      if (!input.value) {
+        input.value = '+7(';
+      } else {
+        return;
+      }
+    });
+
+    input.addEventListener('input', function () {
+      var numberLength = input.value.length;
+
+      if (numberLength === 6) {
+        input.value = input.value + ')';
+      }
+    });
+  };
+
+  for (var i = 0; i < phoneNumber.length; i++) {
+    setMask(phoneNumber[i]);
+  }
+})();
