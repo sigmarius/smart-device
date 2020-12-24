2c57e8b636571159585cce0bc4e77697fd2c32a2
sigmarius <aamokeeva@yandex.ru>
2020-12-24 23:54:13 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,88 @@
 'use strict';
 
+// меняет текст в span на .. в разделе О компании на планшете и мобильном
+
+(function () {
+  var WORDS_COUNT = 45;
+  var DEVIDER = '..';
+
+  var tablet = window.matchMedia('(max-width: 1023px)');
+
+  var textContainer = document.querySelector('.about__description');
+
+  var originalText = textContainer.innerText;
+
+  var smallText = originalText.split(' ', WORDS_COUNT).join(' ').concat(DEVIDER);
+
+  if (textContainer) {
+    var changeTextHandler = function (evt) {
+      if (evt.matches) {
+        textContainer.innerText = smallText;
+      } else {
+        textContainer.innerText = originalText;
+      }
+    };
+  }
+
+  tablet.addListener(changeTextHandler);
+  changeTextHandler(tablet);
+
+})();
+
+'use strict';
+
+// аккордион на mobile в футере
+(function () {
+  var accordion = document.querySelectorAll('.accordion');
+
+  if (accordion) {
+    for (var i = 0; i < accordion.length; i++) {
+      accordion[i].addEventListener('click', function (evt) {
+        var button = evt.target;
+        button.classList.toggle('accordion--active');
+
+        var panel = button.parentNode.nextElementSibling;
+
+        if (panel.classList.contains('accordion__panel--visible')) {
+          panel.classList.remove('accordion__panel--visible');
+          panel.classList.add('accordion__panel--hidden');
+        } else {
+          panel.classList.add('accordion__panel--visible');
+          panel.classList.remove('accordion__panel--hidden');
+        }
+      });
+    }
+  }
+
+})();
+
+'use strict';
+
+// Плавная прокрутка до якоря
+(function () {
+  var links = document.querySelectorAll('[data-link]');
+
+  if (links) {
+    for (var i = 0; i < links.length; i++) {
+
+      links[i].addEventListener('click', function (evt) {
+
+        var target = document.querySelector('[id=' + evt.target.dataset.link + ']');
+
+        if (target) {
+          target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+          });
+        }
+      });
+    }
+  }
+
+})();
+
+'use strict';
+
 // меняет текст кнопки на mobile на главном экране hero
 (function () {
   var mobile = window.matchMedia('(max-width: 767px)');
@@ -18,62 +101,14 @@
 
 })();
 
-// меняет текст в span на .. в разделе О компании на планшете и мобильном
-
-(function () {
-  var WORDS_COUNT = 45;
-  var DEVIDER = '..';
-
-  var tablet = window.matchMedia('(max-width: 1023px)');
-
-  var textContainer = document.querySelector('.about__description');
-
-  var originalText = textContainer.innerText;
-
-  var smallText = originalText.split(' ', WORDS_COUNT).join(' ').concat(DEVIDER);
-
-  var changeTextHandler = function (evt) {
-    if (evt.matches) {
-      textContainer.innerText = smallText;
-    } else {
-      textContainer.innerText = originalText;
-    }
-  };
-
-  tablet.addListener(changeTextHandler);
-  changeTextHandler(tablet);
-
-})();
-
-// аккордион на mobile в футере
-(function () {
-  var accordion = document.querySelectorAll('.accordion');
-
-  for (var i = 0; i < accordion.length; i++) {
-    accordion[i].addEventListener('click', function (evt) {
-      var button = evt.target;
-      button.classList.toggle('accordion--active');
-
-      var panel = button.parentNode.nextElementSibling;
-
-      if (panel.classList.contains('accordion__panel--visible')) {
-        panel.classList.remove('accordion__panel--visible');
-        panel.classList.add('accordion__panel--hidden');
-      } else {
-        panel.classList.add('accordion__panel--visible');
-        panel.classList.remove('accordion__panel--hidden');
-      }
-    });
-  }
-
-})();
+'use strict';
 
 // модальное окно Заказать звонок
 (function () {
   var KeyCode = {
     ESCAPE: 'Escape',
   };
-  var modalOpen = document.querySelector('.modal-open');
+  var modalOpen = document.querySelector('.modal-overlay--open');
   var modalClose = document.querySelector('.modal__close');
   var modal = document.querySelector('.modal');
   var body = document.querySelector('body');
@@ -98,11 +133,11 @@
 
   var setVisible = function (visible) {
     if (visible) {
-      body.classList.add('modal-open');
+      body.classList.add('modal-overlay--open');
       document.addEventListener('keydown', escapeClickHandler);
     } else {
-      body.classList.remove('modal-open');
-      modal.classList.remove('modal-show');
+      body.classList.remove('modal-overlay--open');
+      modal.classList.remove('modal-overlay--show');
       document.removeEventListener('keydown', escapeClickHandler);
     }
   };
@@ -114,66 +149,58 @@
     }
   };
 
-  modal.addEventListener('click', function (evt) {
-    if (evt.target === modal && evt.target !== form) {
-      setVisible(false);
-    }
-  });
-
-  modalOpen.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    modal.classList.add('modal-show');
-    setVisible(true);
-    name.focus();
-
-    if (nameStorage && phoneStorage) {
-      name.value = nameStorage;
-      phone.value = phoneStorage;
-      comment.value = commentStorage;
-    }
-  });
-
-  modalClose.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    modal.classList.remove('modal-show');
-    setVisible(false);
-  });
-
-  form.addEventListener('submit', function (evt) {
-    if (!name.value || !phone.value) {
-      evt.preventDefault();
-    } else {
-      if (isStorage) {
-        localStorage.setItem('nameStorage', name.value);
-        localStorage.setItem('phoneStorage', phone.value);
-        localStorage.setItem('commentStorage', comment.value);
+  if (modal) {
+    modal.addEventListener('click', function (evt) {
+      if (evt.target === modal && evt.target !== form) {
+        setVisible(false);
       }
-    }
-  });
+    });
+  }
 
-})();
+  if (modalOpen) {
+    modalOpen.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      if (modal) {
+        modal.classList.add('modal-overlay--show');
+        setVisible(true);
+        name.focus();
+      }
 
-// Плавная прокрутка до якоря
-(function () {
-  var links = document.querySelectorAll('[data-link]');
+      if (nameStorage && phoneStorage) {
+        name.value = nameStorage;
+        phone.value = phoneStorage;
+        comment.value = commentStorage;
+      }
+    });
+  }
 
-  for (var i = 0; i < links.length; i++) {
+  if (modalClose) {
+    modalClose.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      modal.classList.remove('modal-overlay--show');
+      setVisible(false);
+    });
+  }
 
-    links[i].addEventListener('click', function (evt) {
-
-      var target = document.querySelector('[id=' + evt.target.dataset.link + ']');
-
-      target.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-      });
+  if (form) {
+    form.addEventListener('submit', function (evt) {
+      if (!name.value || !phone.value) {
+        evt.preventDefault();
+      } else {
+        if (isStorage) {
+          localStorage.setItem('nameStorage', name.value);
+          localStorage.setItem('phoneStorage', phone.value);
+          localStorage.setItem('commentStorage', comment.value);
+        }
+      }
     });
   }
 
 })();
 
+'use strict';
 
-// Валидация номера телефона
+// Валидация номера телефона в модальном окне
 (function () {
   var phoneNumber = document.querySelectorAll('[type=tel]');
 
@@ -195,7 +222,10 @@
     });
   };
 
-  for (var i = 0; i < phoneNumber.length; i++) {
-    setMask(phoneNumber[i]);
+  if (phoneNumber) {
+    for (var i = 0; i < phoneNumber.length; i++) {
+      setMask(phoneNumber[i]);
+    }
   }
+
 })();
