2e418688ed0f7fc0ef1fa998a4b2a553d052e678
sigmarius <aamokeeva@yandex.ru>
2020-12-25 00:13:19 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// меняет текст в span на .. в разделе О компании на планшете и мобильном
+// меняет текст в последнем параграфе на .. в разделе О компании на планшете и мобильном
 
 (function () {
   var WORDS_COUNT = 45;
@@ -78,26 +78,6 @@
       });
     }
   }
-
-})();
-
-'use strict';
-
-// меняет текст кнопки на mobile на главном экране hero
-(function () {
-  var mobile = window.matchMedia('(max-width: 767px)');
-  var consultationButton = document.querySelector('.js-consultation');
-
-  var changeTextHandler = function (evt) {
-    if (evt.matches) {
-      consultationButton.textContent = 'Бесплатная консультация';
-    } else {
-      consultationButton.textContent = 'Получить бесплатную консультацию';
-    }
-  };
-
-  mobile.addListener(changeTextHandler);
-  changeTextHandler(mobile);
 
 })();
 
