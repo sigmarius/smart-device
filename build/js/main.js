c1bda4c8b1cf631db67572ae88b95314b6b76850
sigmarius <aamokeeva@yandex.ru>
2020-12-28 20:47:34 +0300

    Откатывается назад, так как не в ту сторону пошли

--- build/js/main.js
+++ build/js/main.js
@@ -10,38 +10,22 @@
 }());
 // меняет текст в последнем параграфе на .. в разделе О компании на планшете и мобильном
 (function () {
-    var WORDS_COUNT = 46;
+    var WORDS_COUNT = 45;
     var DEVIDER = '..';
     var tablet = window.matchMedia('(max-width: 1023px)');
     var textContainer = document.querySelector('.about__description');
     var originalText = textContainer.innerText;
-    var smallText = originalText.split(' ', WORDS_COUNT).join(' ').concat(DEVIDER);
-    var paragraphs = originalText.split('. ');
-    textContainer.innerHTML = '';
-    var first = paragraphs[0] + '. ' + paragraphs[1] + '. ';
-    var second = paragraphs[2] + '. ' + paragraphs[3] + '. ' + paragraphs[4];
-    var newParagraphs = [first, second];
-    var createParagraph = function (elem) {
-        var p = document.createElement('p');
-        p.textContent = elem;
-        textContainer.appendChild(p);
-    };
-    for (var i = 0; i < newParagraphs.length; i++) {
-        createParagraph(newParagraphs[i]);
-    }
-    console.log(newParagraphs);
-    console.log(smallText);
-    if (textContainer) {
-        var changeTextHandler = function (evt) {
-            if (evt.matches) {
-                textContainer.innerText = smallText;
-            } else {
-                textContainer.innerText = originalText;
-            }
-        };
-    }
-    tablet.addListener(changeTextHandler);
-    changeTextHandler(tablet);
+    var smallText = originalText.split(' ', WORDS_COUNT).join(' ').concat(DEVIDER);    // if (textContainer) {
+                                                                                       //   var changeTextHandler = function (evt) {
+                                                                                       //     if (evt.matches) {
+                                                                                       //       textContainer.innerText = smallText;
+                                                                                       //     } else {
+                                                                                       //       textContainer.innerText = originalText;
+                                                                                       //     }
+                                                                                       //   };
+                                                                                       // }
+                                                                                       // tablet.addListener(changeTextHandler);
+                                                                                       // changeTextHandler(tablet);
 }());
 // аккордион на mobile в футере
 (function () {
--- source/js/about-description.js
+++ source/js/about-description.js
@@ -1,9 +1,10 @@
+  
 'use strict';
 
 // меняет текст в последнем параграфе на .. в разделе О компании на планшете и мобильном
 
 (function () {
-  var WORDS_COUNT = 46;
+  var WORDS_COUNT = 45;
   var DEVIDER = '..';
 
   var tablet = window.matchMedia('(max-width: 1023px)');
@@ -14,40 +15,17 @@
 
   var smallText = originalText.split(' ', WORDS_COUNT).join(' ').concat(DEVIDER);
 
-  var paragraphs = originalText.split('. ');
+  // if (textContainer) {
+  //   var changeTextHandler = function (evt) {
+  //     if (evt.matches) {
+  //       textContainer.innerText = smallText;
+  //     } else {
+  //       textContainer.innerText = originalText;
+  //     }
+  //   };
+  // }
 
-  textContainer.innerHTML = '';
-
-  var first = paragraphs[0]+ '. ' +paragraphs[1] + '. ';
-  var second = paragraphs[2]+ '. ' +paragraphs[3] + '. ' + paragraphs[4];
-
-  var newParagraphs = [first, second];
-
-  var createParagraph = function(elem) {
-    var p = document.createElement('p');
-    p.textContent = elem;
-    textContainer.appendChild(p);
-  };
-
-  for (var i = 0; i < newParagraphs.length; i++) {
-    createParagraph(newParagraphs[i]);
-  }
-
-  console.log(newParagraphs);
-  
-  console.log(smallText);
-
-  if (textContainer) {
-    var changeTextHandler = function (evt) {
-      if (evt.matches) {
-        textContainer.innerText = smallText;
-      } else {
-        textContainer.innerText = originalText;
-      }
-    };
-  }
-
-  tablet.addListener(changeTextHandler);
-  changeTextHandler(tablet);
+  // tablet.addListener(changeTextHandler);
+  // changeTextHandler(tablet);
 
 })();
