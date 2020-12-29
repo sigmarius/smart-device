9cbcf620122d25200511c59d85df533bffbb934e
sigmarius <aamokeeva@yandex.ru>
2020-12-29 23:03:14 +0300

    Исправляет ошибки, связанные с аккордеоном

--- source/js/accordion.js
+++ source/js/accordion.js
@@ -3,24 +3,24 @@
 // аккордион на mobile в футере
 (function () {
   var accordion = document.querySelectorAll('.accordion');
+  var panels = document.querySelectorAll('.accordion__panel');
 
   document.querySelector('.accordion__panel--no-js').classList.remove('accordion__panel--no-js');
 
   if (accordion) {
     for (var i = 0; i < accordion.length; i++) {
       accordion[i].addEventListener('click', function (evt) {
+
+        for (var j = 0; j < panels.length; j++) {
+          panels[j].classList.remove('accordion__panel--active');
+        }
+
         var button = evt.target;
         button.classList.toggle('accordion--active');
 
-        var panel = button.parentNode.nextElementSibling;
+        var panel = button.nextElementSibling;
+        panel.classList.toggle('accordion__panel--active');
 
-        if (button.classList.contains('accordion--active')) {
-          panel.classList.add('accordion__panel--visible');
-          panel.classList.remove('accordion__panel--hidden');
-        } else {
-          panel.classList.add('accordion__panel--hidden');
-          panel.classList.remove('accordion__panel--visible');
-        }
       });
     }
   }
